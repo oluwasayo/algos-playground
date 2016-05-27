d159e7cf96b68a821668bc536f1c757e2c0112d5
Sayo Oladeji <oladejioluwasayo@gmail.com>
2016-05-28 00:03:11 +0100

    Problem171. Solved.

--- LeetCode/Problem171_excel_sheet_column_number.java
+++ LeetCode/Problem171_excel_sheet_column_number.java
@@ -1,12 +1,10 @@
 public class Solution {
     public int titleToNumber(String s) {
         int sum = 0;
-        for (int a = s.length() - 1; a >= 0; a--) sum += val(s.charAt(a)) * pow(26, a);
+        for (int a = 0; a < s.length(); a++) sum += (s.charAt(a) - 'A' + 1) * pow(26, s.length() - a - 1);
         return sum;
     }
-    private int val(char c) {
-        return c - 'A' + 1;
-    }
+
     private int pow(int base, int exponent) {
         int result = 1;
         for (int a = 0; a < exponent; a++) result *= base;
