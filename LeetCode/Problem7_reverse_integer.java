7e7842ce205645f7ceed39e4faeeed18c0e27458
Sayo Oladeji <oladejioluwasayo@gmail.com>
2017-05-29 23:25:57 +0100

    Update Problem7_reverse_integer.java
    Added a more efficient algorithm to Problem7 solution. O(1) space and O(log n) time where n is the number to be reversed.

--- LeetCode/Problem7_reverse_integer.java
+++ LeetCode/Problem7_reverse_integer.java
@@ -1,5 +1,30 @@
+import static java.lang.Math.*;
+
 public class Solution {
+
+    // Constant space, logarithmic time.
     public int reverse(int x) {
+        boolean isNegative = x < 0;
+        x = abs(x);
+        int log = (int) log10(x);
+        int result = 0;
+        for (int a = log; a >= 0; a--) {
+            int divisor = (int) pow(10, a);
+            try {
+                int part = (int) ((x / divisor)* pow(10, log - a));
+                result = addExact(result, part);
+                if (isNegative) addExact(0 - result, part);
+            } catch (ArithmeticException ex) {
+                return 0;
+            }
+            x %= divisor;
+        }
+        
+        return isNegative ? 0 - result : result;
+    }
+
+    // Linear space, logarithmic time.
+    public int reverse2(int x) {
         int num = x;
         StringBuilder result = new StringBuilder();
         char[] chars = Integer.toString(Math.abs(num)).toCharArray();
